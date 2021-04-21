0c261df932ba2220be1e0196d197f492c2f32eec
Andrew Druk <adruk@readdle.com>
2021-04-22 00:43:36 +0300

    Dev: build ICU only

--- build/Linux/020-clone-swift.sh
+++ build/Linux/020-clone-swift.sh
@@ -4,6 +4,6 @@ set -ex
 source $HOME/.build_env
 
 pushd $SWIFT_SRC
-    git clone https://github.com/readdle/swift.git --branch readdle/release/5.4
+    git clone https://github.com/readdle/swift.git --branch readdle/release/5.4 --single-branch
     swift/utils/update-checkout --clone --scheme readdle/release/5.4
 popd
