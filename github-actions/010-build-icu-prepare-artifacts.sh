5e858c75f0176350e46d008670fe72047f0d3347
Andrew Druk <adruk@readdle.com>
2021-04-22 10:40:45 +0300

    Dev: fix permission

--- github-actions/010-build-icu-prepare-artifacts.sh
+++ github-actions/010-build-icu-prepare-artifacts.sh
@@ -5,7 +5,7 @@ source $HOME/.build_env
 
 for arch in arm64-v8a armeabi-v7a x86 x86_64
 do
-	pushd $ICU_LIBS/$arch
-		zip -r icu-$arch.zip include/ lib/ *.so
-	popd
+  pushd $ICU_LIBS/$arch
+    zip -r icu-$arch.zip include/ lib/ *.so
+  popd
 done
